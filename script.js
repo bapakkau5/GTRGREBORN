b22fc60df8d8ec6676dc5077ed4fe73beafdff66
bapakkau5 <68463547+bapakkau5@users.noreply.github.com>
2025-07-28 22:51:12 +0700

    Update and rename script.js to index-en.html

--- script.js
+++
@@ -1,8 +0,0 @@
-// Animasi Fade In
-document.addEventListener("DOMContentLoaded", () => {
-  document.body.style.opacity = "0";
-  setTimeout(() => {
-    document.body.style.transition = "opacity 1.2s";
-    document.body.style.opacity = "1";
-  }, 100);
-});
